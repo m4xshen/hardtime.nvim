2e144f4a929cb2fc550c0c0fd16626528901e468
m4xshen <m4xshen@gmail.com>
2023-04-28 23:59:28 +0800

    Add hint

--- lua/hardtime.lua
+++ lua/hardtime.lua
@@ -8,11 +8,13 @@ local config = {
    max_time = 1000,
    max_count = 2,
    disable_mouse = true,
+   hint = true,
    allow_different_key = false,
    resetting_keys = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" },
    restricted_keys = { "h", "j", "k", "l", "-", "+", "<UP>", "<DOWN>", "<LEFT>", "<RIGHT>" },
+   hint_keys = { "k", "j", "^", "$", "a", "A", "x", "i", "d", "y", "c", "l" },
    disabled_keys = { "<UP>", "<DOWN>", "<LEFT>", "<RIGHT>" },
-   disabled_filetypes = { "NvimTree", "qf" }
+   disabled_filetypes = { "qf", "NvimTree", "lazy", "mason" }
 }
 
 local function get_time()
@@ -29,9 +31,44 @@ local function is_disabled()
    return false
 end
 
+local function contains(array, element)
+   for _, val in ipairs(array) do
+      if val == element then
+         return true
+      end
+   end
+
+   return false
+end
+
 local function handler(key)
    if is_disabled() then
       return key
+   end
+
+   -- hint
+   if contains(config.hint_keys, key) then
+      if last_key == "x" and key == "i" then
+         print("Use s instead of xi")
+      elseif last_key == "k" and key == "^" then
+         print("Use - instead of k^")
+      elseif last_key == "j" and key == "^" then
+         print("Use + instead of j^")
+      elseif last_key == "x" and key == "i" then
+         print("Use s instead of xi")
+      elseif last_key == "c" and key == "l" then
+         print("Use s instead of cl")
+      elseif last_key == "d" and key == "$" then
+         print("Use D instead of d$")
+      elseif last_key == "c" and key == "$" then
+         print("Use C instead of c$")
+      end
+
+      last_key = key
+
+      if not contains(config.restricted_keys, key) then
+         return key
+      end
    end
 
    local curr_time = get_time()
@@ -51,6 +88,7 @@ local function handler(key)
    end
 
    print("You press key " .. key .. " too soon!")
+   last_key = key
    return ""
 end
 
@@ -84,6 +122,13 @@ function hardtime.setup(user_config)
    for _, key in pairs(config.restricted_keys) do
       vim.keymap.set("n", key, function() return handler(key)
          end, { noremap = true, expr = true })
+   end
+
+   if config.hint then
+      for _, key in pairs(config.hint_keys) do
+         vim.keymap.set({"n", "o"}, key, function() return handler(key)
+         end, { noremap = true, expr = true })
+      end
    end
 
    for _, key in pairs(config.disabled_keys) do
