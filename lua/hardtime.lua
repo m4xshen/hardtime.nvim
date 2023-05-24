c462c73192145f434477b1dbe3d19fb795fbe767
m4xshen <m4xshen@gmail.com>
2023-05-24 16:14:33 +0800

    Use handler() for all keys

--- lua/hardtime.lua
+++ lua/hardtime.lua
@@ -1,7 +1,11 @@
 local hardtime = {}
 
-local last_time
-local last_count
+local function get_time()
+   return vim.fn.reltimefloat(vim.fn.reltime()) * 1000
+end
+
+local last_time = get_time()
+local last_count = 0
 local last_key
 
 local config = {
@@ -12,14 +16,10 @@ local config = {
    allow_different_key = false,
    resetting_keys = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "c", "d", "x", "X", "p", "P" },
    restricted_keys = { "h", "j", "k", "l", "-", "+", "<UP>", "<DOWN>", "<LEFT>", "<RIGHT>" },
-   hint_keys = { "k", "j", "^", "$", "a", "x", "i", "d", "y", "c", "l" },
+   hint_keys = { "k", "j", "^", "$", "a", "i", "d", "y", "c", "l" },
    disabled_keys = { "<UP>", "<DOWN>", "<LEFT>", "<RIGHT>" },
    disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason" }
 }
-
-local function get_time()
-   return vim.fn.reltimefloat(vim.fn.reltime()) * 1000
-end
 
 local function is_disabled()
    local current_filetype = vim.api.nvim_buf_get_option(0, "filetype")
@@ -41,44 +41,56 @@ local function contains(array, element)
    return false
 end
 
+local function display_hint(key)
+   if last_key == "k" and key == "^" then
+      print("Use - instead of k^")
+   elseif last_key == "j" and key == "^" then
+      print("Use + instead of j^")
+   elseif last_key == "c" and key == "l" then
+      print("Use s instead of cl")
+   elseif last_key == "d" and key == "$" then
+      print("Use D instead of d$")
+   elseif last_key == "c" and key == "$" then
+      print("Use C instead of c$")
+   elseif last_key == "$" and key == "a" then
+      print("Use A instead of $a")
+   elseif last_key == "^" and key == "i" then
+      print("Use I instead of ^i")
+   end
+end
+
 local function handler(key)
+   -- plugin disabled
    if is_disabled() then
       return key
    end
 
-   -- hint
-   if contains(config.hint_keys, key) then
-      if last_key == "x" and key == "i" then
-         print("Use s instead of xi")
-      elseif last_key == "k" and key == "^" then
-         print("Use - instead of k^")
-      elseif last_key == "j" and key == "^" then
-         print("Use + instead of j^")
-      elseif last_key == "x" and key == "i" then
-         print("Use s instead of xi")
-      elseif last_key == "c" and key == "l" then
-         print("Use s instead of cl")
-      elseif last_key == "d" and key == "$" then
-         print("Use D instead of d$")
-      elseif last_key == "c" and key == "$" then
-         print("Use C instead of c$")
-      elseif last_key == "$" and key == "a" then
-         print("Use A instead of $a")
-      elseif last_key == "^" and key == "i" then
-         print("Use I instead of ^i")
-      end
-
-      if not contains(config.restricted_keys, key) then
-         last_key = key
-
-         return key
-      end
+   -- key disabled
+   if contains(config.disabled_keys, key) then
+      print("Key " .. key .. " is disabled!")
+      return ""
    end
 
+   -- hint
+   if contains(config.hint_keys, key) then
+      display_hint(key)
+   end
+
+   -- reset
+   if contains(config.resetting_keys, key) then
+      last_count = 0
+   end
+
+   if not contains(config.restricted_keys, key) then
+      last_key = key
+      return key
+   end
+
+   -- restrict
    local curr_time = get_time()
 
-   if curr_time - last_time > config.max_time or
-      last_count < config.max_count or
+   if last_count < config.max_count or
+      curr_time - last_time > config.max_time or
       (config.allow_different_key and key ~= last_key) then
       if curr_time - last_time > config.max_time or
          (config.allow_different_key and key ~= last_key) then
@@ -97,12 +109,6 @@ local function handler(key)
    return ""
 end
 
-local function reset()
-   last_time = get_time()
-   last_count = 0
-   last_key = ""
-end
-
 function hardtime.setup(user_config)
    user_config = user_config or {}
 
@@ -110,23 +116,18 @@ function hardtime.setup(user_config)
       config[option] = value
    end
 
-   reset()
-
    if config.disable_mouse then
       vim.opt.mouse = ""
    end
 
    for _, key in pairs(config.resetting_keys) do
-      vim.keymap.set("n", key,
-         function()
-            reset()
-            return key
-         end, { noremap = true, expr = true })
+      vim.keymap.set("n", key, function() return handler(key)
+      end, { noremap = true, expr = true })
    end
 
    for _, key in pairs(config.restricted_keys) do
       vim.keymap.set("n", key, function() return handler(key)
-         end, { noremap = true, expr = true })
+      end, { noremap = true, expr = true })
    end
 
    if config.hint then
@@ -137,9 +138,7 @@ function hardtime.setup(user_config)
    end
 
    for _, key in pairs(config.disabled_keys) do
-      vim.keymap.set({ "", "i" }, key, function()
-         print("Key " .. key .. " is disabled!")
-         return ""
+      vim.keymap.set({ "", "i" }, key, function() return handler(key)
       end, { noremap = true })
    end
 end
