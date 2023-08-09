84566ae0d602b5e32fb0a6695e70db151e29224f
Max Shen <m4xshen@gmail.com>
2023-08-09 21:43:45 +0800

    Add log feature

--- lua/hardtime/util.lua
+++ lua/hardtime/util.lua
@@ -1,6 +1,12 @@
 local M = {}
 local previous_text
 
+local logger = require("hardtime.log").new({
+   plugin = "hardtime.nvim",
+   level = "info",
+   use_console = false,
+})
+
 function M.get_time()
    return vim.fn.reltimefloat(vim.fn.reltime()) * 1000
 end
@@ -15,6 +21,7 @@ end
 
 function M.notify(text)
    if text ~= previous_text then
+      logger.info(text)
       vim.notify(text, vim.log.levels.WARN, { title = "hardtime" })
    end
    previous_text = text
