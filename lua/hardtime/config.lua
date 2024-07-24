3509aa63e27996e1c0372c9500e3ba7f14691f24
Max Shen <m4xshen@gmail.com>
2024-07-24 23:09:27 +0800

    chore: update default

--- lua/hardtime/config.lua
+++ lua/hardtime/config.lua
@@ -2,11 +2,11 @@ local M = {}
 
 M.config = {
    max_time = 1000,
-   max_count = 2,
+   max_count = 3,
    disable_mouse = true,
    hint = true,
    notification = true,
-   allow_different_key = false,
+   allow_different_key = true,
    enabled = true,
    resetting_keys = {
       ["1"] = { "n", "x" },
